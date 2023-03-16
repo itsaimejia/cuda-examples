9de1d87a31ae5ffdec05c155bf6f8ee296d509c6
itsaimejia <itsaizempoaltecatlmejia@gmail.com>
2023-03-16 16:07:54 -0700

    a

--- images/test_derivada_gauss.py
+++ images/test_derivada_gauss.py
@@ -1,21 +1,23 @@
 import cv2
 import numpy as np
 import os
+from numba import jit
 
+@jit(nopython=True)
 def derivada_gaussiana(img):
     # Aplica un filtro de suavizado
-    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img2 = cv2.GaussianBlur(img, (3, 3), 0)
 
     # Calcula las derivadas parciales en la dirección horizontal y vertical
-    dx = cv2.Sobel(img, cv2.CV_64F, 1, 0)
-    dy = cv2.Sobel(img, cv2.CV_64F, 0, 1)
+    dx = cv2.Sobel(img2, cv2.CV_64F, 1, 0)
+    dy = cv2.Sobel(img2, cv2.CV_64F, 0, 1)
 
     # Calcula la magnitud y dirección de la gradiente
     mag = np.sqrt(dx**2 + dy**2)
     ang = np.arctan2(dy, dx)
 
     # Normaliza la magnitud a un rango de 0 a 255
-    return cv2.normalize(mag, ang, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+    return cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
 
 file_name = os.path.join(os.path.dirname(__file__), 'avengers.jpg')
 assert os.path.exists(file_name)
